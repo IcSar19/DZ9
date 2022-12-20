9e079523c7d8337774fb7b81345cd8b378b2c202
Daniel <daniel.bozhko1903@mail.ru>
2022-12-20 17:27:32 +0700

    Result

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+void Number(int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    Number(number - 1);
+}
+
+Number(number);
--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintSum(m, n));
+
+int PrintSum(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;
+    else if (n == 0) return (m * (m + 1)) / 2;
+    else if (m == n) return m;
+    else if (m < n) return n + PrintSum(m, n - 1);
+    else return n + PrintSum(m, n + 1);
+}
